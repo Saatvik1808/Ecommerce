784fb8f1a76e6981a9c9c7dff3592f451a645100
Saatvik1808 <saatvik.shrivastava08@gmail.com>
2023-07-19 15:08:15 +0530

    panel

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -11,4 +11,11 @@ const router=express.Router();
 router.post('/register',registerController)
 router.post('/login',loginController)
 router.get("/test", requireSignIn,isAdmin,testController);
+router.get("/user-auth", requireSignIn, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
+  //protected Admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+    res.status(200).send({ ok: true });
+  });
 export default router
