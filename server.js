bec1e9e4ee9a251d7d0532ae59aab263e9b4e00a
Saatvik1808 <saatvik.shrivastava08@gmail.com>
2023-08-04 14:05:32 +0530

    deploy

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ import  authRoute from './routes/authRoute.js'
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors'
+import path from 'path'
 dotenv.config();
 connectDB(); 
 const app=express()
 app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use("/api/v1/auth",authRoute)
 // rest api
-app.get('/',(req,res)=>{
-    res.send(
-        "<h1>welcome to ecommerce</h1>"
-    )
-});
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/product", productRoutes);
 
