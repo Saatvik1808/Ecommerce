507b22b09fb85c6f76818944c58d213a5e9c3da2
Saatvik1808 <saatvik.shrivastava08@gmail.com>
2023-08-04 14:09:47 +0530

    updated

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors'
 import path from 'path'
+import {fileURLToPath} from 'url'
+
 dotenv.config();
 connectDB(); 
+
+const_filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 const app=express()
 app.use(cors());
 app.use(express.json())
