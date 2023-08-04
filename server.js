75e349e03419a0840483ab9d2b161efd0815ca7e
Saatvik1808 <saatvik.shrivastava08@gmail.com>
2023-08-04 14:13:49 +0530

    fixed

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import {fileURLToPath} from 'url'
 dotenv.config();
 connectDB(); 
 
-const_filename=fileURLToPath(import.meta.url);
+const __filename=fileURLToPath(import.meta.url);
 const __dirname=path.dirname(__filename);
 const app=express()
 app.use(cors());
